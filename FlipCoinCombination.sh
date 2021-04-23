390db1edd62cccdea487ed0099a4948dc6ab9e4c
Megha <meghamoree@gmail.com>
2021-04-23 13:43:30 +0530

    Added: Singlet Combination in a dictionary

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -x
 
-coinFlip=$((RANDOM%2))
+declare -A dict
 
-if [ $coinFlip -eq 1 ]
-then
-	echo HEADS
-else
-	echo TAILS
-fi
+echo "Welcome to flipCoin simulator "
+read -p "Enter the number of times coin to be flipped " num
 
+
+for(( i = 0; i < num; i++ ))
+do
+        checkRandom=$((RANDOM%2))
+        if [ $checkRandom -eq 0 ]
+        then
+                dict[heads]=$((${dict[heads]}+1))
+                Hpercentage=$((${dict[heads]}*100/num))
+        else
+                dict[tails]=$((${dict[tails]}+1))
+                Tpercentage=$((${dict[tails]}*100/num))
+        fi
+done
+
+echo  "Singlet outcome is tail percent is $Tpercentage" "head percent is $Hpercentage"
